c9e724efdaafeba5c6a73737f5a33e7222a453fb
kazutaka8 <blitzaus825@gmail.com>
2025-08-30 12:03:49 +0900

    返信内容変更

--- app.py
+++ app.py
@@ -34,7 +34,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+    recieved_msg = event.message.text
+    reply_msg = ("！".join(recieved_msg), "！")
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
 
 
 if __name__ == "__main__":
